e4bfc71d7105bc5c4fc6a11c73cbc241fefb7c2d
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-05-14 20:13:35 -0500

    tag creation working

--- script.js
+++ script.js
@@ -12,4 +12,13 @@ function createTags(input) {
     .split(",")
     .filter((tag) => tag.trim() !== "")
     .map((tag) => tag.trim());
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
 }
