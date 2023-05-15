350ae5f9f33c512d18b770f226d38424e328b8c8
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-05-14 20:08:46 -0500

    split input into array and seperated

---
+++ script.js
@@ -0,0 +1,15 @@
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+}
